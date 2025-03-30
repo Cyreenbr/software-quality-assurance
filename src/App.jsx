1184646b94f9b332cfeaae480502302b77bfddf9
noursaidane2001 <saidanenour2001@gmail.com>
2025-03-30 11:39:01 +0100

    SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import Notifications from './pages/Notifications';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Subjects from './pages/Subjects';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Notifications from "./pages/Notifications";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/signinPage/SignIn";
+import SignUp from "./pages/signupPage/SignUp";
+import Subjects from "./pages/Subjects";
 
 // Higher-order component to wrap pages with Layout
 const withLayout = (Component) => {
@@ -26,10 +26,22 @@ function App() {
         {/* NB : essta3eml withLayout() ken hajtek bech tzid menu w header lel page te3k kima l'exemple louta */}
         {/* <Route path="/route" element={React.createElement(withLayout(Element_li_t7eb_tzidou_header_w_menu))} /> */}
         <Route path="/" element={React.createElement(withLayout(Home))} />
-        <Route path="/dashboard" element={React.createElement(withLayout(Dashboard))} />
-        <Route path="/profile" element={React.createElement(withLayout(Profile))} />
-        <Route path="/subjects" element={React.createElement(withLayout(Subjects))} />
-        <Route path="/notifications" element={React.createElement(withLayout(Notifications))} />
+        <Route
+          path="/dashboard"
+          element={React.createElement(withLayout(Dashboard))}
+        />
+        <Route
+          path="/profile"
+          element={React.createElement(withLayout(Profile))}
+        />
+        <Route
+          path="/subjects"
+          element={React.createElement(withLayout(Subjects))}
+        />
+        <Route
+          path="/notifications"
+          element={React.createElement(withLayout(Notifications))}
+        />
 
         {/* Pages without Layout */}
         <Route path="/signin" element={<SignIn />} />
