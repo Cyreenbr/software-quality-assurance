6aa794f2703a5791be97b41a71d1e579fe816430
Raja <rajahannachi21@gmail.com>
2025-04-02 16:21:30 +0100

    before_merging

--- src/services/configs/menuHandler.js
+++ src/services/configs/menuHandler.js
@@ -1,14 +1,20 @@
-
-import { FaBook } from "react-icons/fa";
-import { MdAccountCircle, MdDashboard, MdHome, MdNotifications } from "react-icons/md";
+import { FaBook, FaGraduationCap, FaLightbulb } from 'react-icons/fa';
+import { GiTeacher } from "react-icons/gi";
+import { MdAccountCircle, MdDashboard, MdHome, MdNotifications, MdSchool } from 'react-icons/md';
+import { PiStudentFill } from "react-icons/pi";
+import Pfa from '../../../src/pages/pfaPage/Pfa';
+import SubjectDetailsPage from '../../components/subjectsComponents/SubjectDetailsPage';
+import Competences from '../../pages/competenecesPage/Competences';
 import Dashboard from '../../pages/dashboardPage/Dashboard';
-import ErrorPage from "../../pages/ErrorPage";
+import ErrorPage from '../../pages/ErrorPage';
 import Home from '../../pages/homePage/Home';
 import Notifications from '../../pages/Notifications';
 import Profile from '../../pages/profilePage/Profile';
+import StudentManag from '../../pages/StudentManag';
 import Subjects from '../../pages/subjectsPage/Subjects';
+import StudentsManagPage from "../../pages/UsersManagementPage/StudentsManagPage";
+import TeachersManagPage from "../../pages/UsersManagementPage/TeachersManagPage";
 import { RoleEnum } from "../../utils/userRoles";
-
 export const menuConfig = [
     {
         order: 1,
@@ -103,6 +109,91 @@ export const menuConfig = [
         hideSideBar: false,
         hideHeader: false,
     },
+    {
+        order: 8,
+        label: 'PFA',
+        icon: FaGraduationCap,
+        path: '/PFA',
+        component: Pfa,
+        eligibleRoles: [RoleEnum.ADMIN, RoleEnum.TEACHER, RoleEnum.STUDENT],
+        active: true,
+        hideSideBar: false,
+        hideHeader: false,
+
+    },
+    {
+        order: 9,
+        label: 'Student Management',
+        icon: MdSchool,
+        path: '/student-management',
+        component: StudentManag,
+        eligibleRoles: [RoleEnum.ADMIN],
+        active: true,
+        hideSideBar: false,
+        hideHeader: false,
+
+    },
+    {
+        order: 10,
+        label: 'Manage Students',
+        icon: PiStudentFill,
+        path: '/managestudents',
+        component: StudentsManagPage,
+        eligibleRoles: [RoleEnum.ADMIN],
+        active: true,
+        hideSideBar: false,
+        hideHeader: false,
+    },
+    {
+        order: 11,
+        label: 'Manage Teachers',
+        icon: GiTeacher,
+        path: '/manageteachers',
+        component: TeachersManagPage,
+        eligibleRoles: [RoleEnum.ADMIN],
+        active: true,
+        hideSideBar: false,
+        hideHeader: false,
+    },
+    {
+        order: 12,
+        label: 'Period',
+        icon: MdViewAgenda,
+        path: '/period',
+        tooltip: "period",
+        component: AdminPeriods,
+        eligibleRoles: [RoleEnum.ADMIN],
+        active: true,
+        dontShow: false,
+        hideSideBar: false,
+        hideHeader: false,
+    },
+     {
+        order: 13,
+        label: 'Depot',
+        icon: MdDownload,
+        path: '/deposit',
+        tooltip: "depot",
+        component: DepotSujet,
+        eligibleRoles: [RoleEnum.STUDENT],
+        active: true,
+        dontShow: false,
+        hideSideBar: false,
+        hideHeader: false,
+    },
+    {   order: 14,
+        label: 'Internship',
+        icon: MdList,
+        path: '/InternshipList',
+        tooltip: "Internship",
+        component: InternshipList,
+        eligibleRoles: [RoleEnum.ADMIN],
+        active: true,
+        dontShow: false,
+        hideSideBar: false,
+        hideHeader: false,
+    },
+
     // kifeh tzid route jdid ??
     // {
     //     order: 7,
@@ -115,9 +206,10 @@ export const menuConfig = [
     //     hideSideBar: false,
     //     hideHeader: false,
     // },
+
 ];
 
-export const getMenuItems = (role) => {
+ export const getMenuItems = (role) => {
     return menuConfig
         .filter((item) => item?.dontShow !== true)
         .filter((item) => item.eligibleRoles.length === 0 || item.eligibleRoles.includes(role))
