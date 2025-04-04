a6a787748ea677944de6a558e01b43fa67199fcb
OneEyeX <chedly199@gmail.com>
2025-04-04 04:49:35 +0100

    fixed small issue

--- src/services/matieresServices/matieres.service.js
+++ src/services/matieresServices/matieres.service.js
@@ -1,7 +1,7 @@
 import axiosAPI from "../axiosAPI/axiosInstance";
 
 const matieresServices = {
-    fetchMatieres: async (page = 1, searchTerm = '', sortBy = '_id', order = 'desc', limit = 10) => {
+    fetchMatieres: async (page = 1, searchTerm = '', sortBy = '_id', order = 'desc', limit = 9) => {
         try {
             const response = await axiosAPI.get("/matieres", {
                 params: { page, limit, searchTerm, sortBy, order }
