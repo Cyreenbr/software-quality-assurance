0cf0ed08a41f209f8e95498d1f39013a8e71a5f2
soltaniahmed0 <ahmed0soltani@gmail.com>
2025-04-07 04:49:39 +0100

    toast

--- src/services/pfeService/pfe.service.js
+++ src/services/pfeService/pfe.service.js
@@ -14,10 +14,8 @@ export const createPFE = async (formData) => {
     toast.success("PFE created successfully");
     return response.data;
   } catch (error) {
-    console.error(
-      " Error creating PFE:",
-      error.response?.data || error.message
-    );
+    toast.error(" Error creating PFE:", error.response?.data || error.message);
+
     throw error;
   }
 };
@@ -38,10 +36,8 @@ export const updatePFE = async (id, formData) => {
     toast.success("PFE updated successfully");
     return response.data;
   } catch (error) {
-    console.error(
-      " Error updating PFE:",
-      error.response?.data || error.message
-    );
+    toast.error(" Error updating PFE:", error.response?.data || error.message);
+
     throw error;
   }
 };
@@ -66,10 +62,6 @@ export const getPfeList = async () => {
     console.log("PFE List Response Data:", response.data);
     return response.data;
   } catch (error) {
-    console.error(
-      " Error loading PFE list:",
-      error.response?.data || error.message
-    );
     throw error;
   }
 };
@@ -79,10 +71,6 @@ export const choosePfe = async (id) => {
     const response = await axiosAPI.patch(`${API_URL}/${id}/choice`);
     return response.data;
   } catch (error) {
-    console.error(
-      " Error choosing PFE:",
-      error.response?.data || error.message
-    );
     throw error;
   }
 };
@@ -100,8 +88,7 @@ export const handleAction = async (id, action) => {
 
     return response.data;
   } catch (error) {
-    toast.error(` ${error.response?.data?.message || error.message}`);
-    console.error(
+    toast.error(
       ` Error handling ${action} on PFE:`,
       error.response?.data || error.message
     );
@@ -118,10 +105,11 @@ export const assignPFEManually = async (id, teacherId) => {
     console.log(" PFE manually assigned to teacher:", response.data);
     return response.data;
   } catch (error) {
-    console.error(
-      " Error assigning PFE manually:",
+    toast.error(
+      `Error assigning PFE manually:`,
       error.response?.data || error.message
     );
+
     throw error;
   }
 };
@@ -134,7 +122,7 @@ export const togglePublication = async (isPublished) => {
     toast.success(`Planning ${isPublished ? "hidden" : "published"}`);
     return response.data;
   } catch (error) {
-    console.error(
+    toast.error(
       " Error toggling publication status:",
       error.response?.data || error.message
     );
@@ -150,10 +138,7 @@ export const sendEmail = async (type) => {
     toast.success(` ${type === "first" ? "First" : "Modified"} email sent.`);
     return response.data;
   } catch (error) {
-    console.error(
-      " Error sending email:",
-      error.response?.data || error.message
-    );
+    toast.error(" Error sending email:", error.response?.data || error.message);
     throw error;
   }
 };
@@ -164,7 +149,10 @@ export const fetchPFEChoices = async () => {
 
     return response.data;
   } catch (error) {
-    console.error("Error fetching PFE data:", error);
+    toast.error(
+      "Error fetching PFE data:",
+      error.response?.data || error.message
+    );
     throw error;
   }
 };
@@ -174,7 +162,10 @@ export const fetchTeachers = async () => {
     const response = await axiosAPI.get("http://localhost:3000/api/teachers");
     return response.data.model || [];
   } catch (error) {
-    console.error("Error fetching teachers:", error);
+    toast.error(
+      "Error fetching teachers:",
+      error.response?.data || error.message
+    );
     throw error;
   }
 };
@@ -185,13 +176,6 @@ export const getPlanning = async () => {
 
     return response.data;
   } catch (error) {
-    if (error.response) {
-      console.error("Server Error:", error.response.data);
-    } else if (error.request) {
-      console.error("No response from server:", error.request);
-    } else {
-      console.error("Request setup error:", error.message);
-    }
-    throw error;
+    toast.error(error.response?.data || error.message);
   }
 };
