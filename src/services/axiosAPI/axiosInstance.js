5c644215269fd24cfb161f819d89a9906b4f7593
OneEyeX <chedly199@gmail.com>
2025-04-07 03:36:38 +0100

    fixed some stuff

--- src/services/axiosAPI/axiosInstance.js
+++ src/services/axiosAPI/axiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 
 
@@ -16,7 +17,11 @@ const axiosAPI = axios.create({
 
 // Add Authorization header if token exists
 axiosAPI.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+    // const token = localStorage.getItem("token");
+
+    const token = useSelector((state) => state.auth.token) || null;
+    console.log(token);
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
