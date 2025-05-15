40016b2722a4f582024a640a214fb4c10f8c186b
noursaidane2001 <saidanenour2001@gmail.com>
2025-05-16 00:20:03 +0100

    Version1Season

--- src/pages/universityYearPage/UniversityYearPage.jsx
+++ src/pages/universityYearPage/UniversityYearPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UniversityYears from "../../components/UniversityYear/UniversityYears";
+import NewYearListing from "../../components/UniversityYear/NewYearListing";
+import Season from "../../components/UniversityYear/Season";
 const UniversityYearPage = () => {
   return (
     <div>
-      <UniversityYears />
+      <Season />
     </div>
   );
 };
